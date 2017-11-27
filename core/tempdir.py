2d0a8fd00fd8f8604585a28c46ed9682ce5350fd
Gaetan Guidet <gatgui@gmail.com>
2017-11-27 14:28:21 +0900

    rebase adjustments, this commit should now perfectly match official repository 2.2.6 tag

--- core/tempdir.py
+++
@@ -1,2 +0,0 @@
-#Embedded file name: C:/Users/hovel/Dropbox/packages/studiolibrary/1.23.2/build27/studiolibrary\core\tempdir.py
-pass
--- packages/studioqt/action/__init__.py
+++
@@ -1,2 +0,0 @@
-#Embedded file name: C:/Users/hovel/Dropbox/packages/studiolibrary/1.23.2/build27/studiolibrary/packages/studioqt\action\__init__.py
-pass
